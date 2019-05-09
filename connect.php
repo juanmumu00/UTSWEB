3d6a73d03d53fb8aacb2d43d582a7016a61e11e6
juanmumu00 <50292039+juanmumu00@users.noreply.github.com>
2019-05-09 22:17:27 +0700

    menambahkan beberapa file

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$server 	= "localhost";
+$user		= "root";
+$pass		= "";
+$database	= "uts";
+
+$conn = mysqli_connect($server,$user,$pass,$database);
+
+//mysql_connect($server,$user,$pass);
+//mysql_select_db($database);
+
+?>
